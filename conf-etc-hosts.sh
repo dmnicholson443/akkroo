1b7891369bda6f6512a2f6ff06307758d3c04004
DarrenNicholson <dmnicholson443@gmail.com>
2018-08-09 11:46:26 +0100

    created conf-etc-hosts file, which will add vagrant guest names and IP's to /etc/hosts file

---
+++ conf-etc-hosts.sh
@@ -0,0 +1,3 @@
+printf "%s\t%s\n" "machine1" "172.16.100.101" | tee -a /etc/hosts > /dev/null
+printf "%s\t%s\n" "machine2" "172.16.100.102" | tee -a /etc/hosts > /dev/null
+printf "%s\t%s\n" "machine3" "172.16.100.103" | tee -a /etc/hosts > /dev/null
